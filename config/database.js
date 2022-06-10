ce569c90e307b780692d36461025aace91a8608a
Yunus Emre Acar <me@yunusacar.dev>
2022-06-11 00:38:11 +0300

    init

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: env("DATABASE_CLIENT", "postgres"),
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'root'),
+      user: env('DATABASE_USERNAME', '123123'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'),
+
+    },
+    debug: false,
+  }
+});
